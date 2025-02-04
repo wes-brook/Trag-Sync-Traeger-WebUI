800c363b15969aca7b607b9753e013aba920b8ee
Wesly Barayuga <86134875+wes-brook@users.noreply.github.com>
2025-02-03 20:04:07 -0800

    merged with dev, commented out backend for now

--- traeger-webUI/services/api.js
+++ traeger-webUI/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3001/api', // Express server URL
-  baseURL: 'https://traeger-webui-backend.onrender.com/api',
+  // baseURL: 'https://traeger-webui-backend.onrender.com/api',
 });
 
 export const getGrillData = async () => {
